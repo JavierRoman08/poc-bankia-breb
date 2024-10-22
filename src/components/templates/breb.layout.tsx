9237f6f0dbc856a4761064e3c05a164e788e82dd
JavierRoman08 <javier.roman@pragma.com.co>
2024-10-22 12:40:46 -0500

    styles layout

--- src/components/templates/breb.layout.tsx
+++ src/components/templates/breb.layout.tsx
@@ -1,18 +1,9 @@
 import BreBNavbar from "../molecules/bre-b-navbar/bre-b.navbar";
-
-const styles = {
-  width: '100%',
-  height: '100%',
-  minHeight: '100vh',
-  maxWidth: '480px',
-  margin: 'auto',
-  display: 'flex', 
-  flexDirection: 'column'
-}
+import styles from './breb.layout.module.scss';
 
 function BreBLayout({children}: any) {
   return (
-    <main style={styles}>
+    <main className={styles.container}>
       <BreBNavbar />
       <section className="container" style={{flex: 1 }}>
         {children}
